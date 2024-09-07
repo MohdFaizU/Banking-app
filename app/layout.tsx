243f4f8c4849fcdf6a9bbed310f047c62d8b2893
MohdFaizU <faizbpl1307@gmail.com>
2024-09-07 16:02:19 -0400

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import {Inter, IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({subsets: ["latin"], variable: "--font-inter"});
+const ibmplexserif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"], 
+  variable: "--font-ibmplexserif"
+});
+
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for the next generation of businesses.",
+  icons: {
+    icon: "/icons/logo.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmplexserif.variable}`}>{children}
+      </body>
+    </html>
+  );
+}
